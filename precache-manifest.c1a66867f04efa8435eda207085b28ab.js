c969abb24df3ca95d6d94e89c269e573b1c2473d
Piyush_Code99 <73519381+Piyushkumar99@users.noreply.github.com>
2022-03-26 19:37:34 +0530

    Updates

---
+++ precache-manifest.c1a66867f04efa8435eda207085b28ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a7ffcaddad46c781382313ffaff533",
+    "url": "/firechat/index.html"
+  },
+  {
+    "revision": "49bc077237d7023afa66",
+    "url": "/firechat/static/css/main.9cb7495d.chunk.css"
+  },
+  {
+    "revision": "f52b468efeb7825f8684",
+    "url": "/firechat/static/js/2.ddefda99.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/firechat/static/js/2.ddefda99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49bc077237d7023afa66",
+    "url": "/firechat/static/js/main.120b026b.chunk.js"
+  },
+  {
+    "revision": "87c796bfb919be310e44",
+    "url": "/firechat/static/js/runtime-main.2080e0b9.js"
+  }
+]);
